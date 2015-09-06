2a96955322ad8b0cff194c3d5f655768d180e1e9
Pierre Felgines <pierre.felgines@applidium.com>
2015-09-06 19:03:46 +0200

    Implement start observing and stop observing

--- Classes/FDKeyboardManager.m
+++ Classes/FDKeyboardManager.m
@@ -8,20 +8,49 @@
 
 #import "FDKeyboardManager.h"
 
+@interface FDKeyboardManager () {
+    UITableView * _tableView;
+}
+- (void)_keyboardWillShow:(NSNotification *)notification;
+- (void)_keyboardWillHide:(NSNotification *)notification;
+@end
+
 @implementation FDKeyboardManager
 
 - (instancetype)initWithTableView:(UITableView *)tableView {
     if (self = [super init]) {
-
+        _tableView = tableView;
     }
     return self;
 }
 
 - (void)startObservingKeyboard {
-
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
 }
 
 - (void)endObservingKeyboard {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+}
+
+#pragma mark - Private
+
+- (void)_keyboardWillShow:(NSNotification *)notification {
+
+}
+
+- (void)_keyboardWillHide:(NSNotification *)notification {
 
 }
 
