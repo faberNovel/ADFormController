1e68ed634549724ba46e7bf7e8824f2ac9e5bd19
Pierre Felgines <pierre.felgines@applidium.com>
2015-09-06 19:11:55 +0200

    Finish FDKeyboarManager implementation

--- Classes/FDKeyboardManager.m
+++ Classes/FDKeyboardManager.m
@@ -7,12 +7,15 @@
 //
 
 #import "FDKeyboardManager.h"
+#import "UIView+Responder.h"
 
 @interface FDKeyboardManager () {
     UITableView * _tableView;
 }
 - (void)_keyboardWillShow:(NSNotification *)notification;
 - (void)_keyboardWillHide:(NSNotification *)notification;
+- (UITableViewCell *)_cellFromFirstResponder;
+- (UITableViewCell *)_cellForTextEdigintView:(UIView *)textEditingView;
 @end
 
 @implementation FDKeyboardManager
@@ -47,11 +50,78 @@
 #pragma mark - Private
 
 - (void)_keyboardWillShow:(NSNotification *)notification {
+    UITableViewCell * cell = [self _cellFromFirstResponder];
+    if (cell) {
+        NSDictionary * keyboardInfo = [notification userInfo];
 
+        NSTimeInterval keyboardAnimationDuration = [keyboardInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+        UIViewAnimationCurve keyboardAnimationCurve = [keyboardInfo[UIKeyboardAnimationCurveUserInfoKey] intValue];
+        CGRect keyboardFrameEnd = [keyboardInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+
+        CGRect keyboardFrame = [_tableView.window convertRect:keyboardFrameEnd
+                                                       toView:_tableView.superview];
+
+        CGFloat newBottomInset = _tableView.frame.origin.y + _tableView.frame.size.height - keyboardFrame.origin.y;
+        if (newBottomInset > 0){
+            UIEdgeInsets tableContentInset = _tableView.contentInset;
+            UIEdgeInsets tableScrollIndicatorInsets = _tableView.scrollIndicatorInsets;
+            tableContentInset.bottom = newBottomInset;
+            tableScrollIndicatorInsets.bottom = tableContentInset.bottom;
+
+            [UIView beginAnimations:nil context:nil];
+            [UIView setAnimationDuration:keyboardAnimationDuration];
+            [UIView setAnimationCurve:keyboardAnimationCurve]; {
+                _tableView.contentInset = tableContentInset;
+                _tableView.scrollIndicatorInsets = tableScrollIndicatorInsets;
+                NSIndexPath * selectedRow = [_tableView indexPathForCell:cell];
+                [_tableView scrollToRowAtIndexPath:selectedRow
+                                  atScrollPosition:UITableViewScrollPositionNone
+                                          animated:NO];
+            } [UIView commitAnimations];
+        }
+    }
 }
 
 - (void)_keyboardWillHide:(NSNotification *)notification {
+    UITableViewCell * cell = [self _cellFromFirstResponder];
+    if (cell) {
+        NSDictionary * keyboardInfo = [notification userInfo];
 
+        NSTimeInterval keyboardAnimationDuration = [keyboardInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+        UIViewAnimationCurve keyboardAnimationCurve = [keyboardInfo[UIKeyboardAnimationCurveUserInfoKey] intValue];
+
+        UIEdgeInsets tableContentInset = _tableView.contentInset;
+        UIEdgeInsets tableScrollIndicatorInsets = _tableView.scrollIndicatorInsets;
+        tableContentInset.bottom = 0;
+        tableScrollIndicatorInsets.bottom = tableContentInset.bottom;
+
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:keyboardAnimationDuration];
+        [UIView setAnimationCurve:keyboardAnimationCurve]; {
+            _tableView.contentInset = tableContentInset;
+            _tableView.scrollIndicatorInsets = tableScrollIndicatorInsets;
+        } [UIView commitAnimations];
+    }
 }
+
+- (UITableViewCell *)_cellFromFirstResponder {
+    UIView * firstResponderView = [_tableView ct_findFirstResponder];
+    if ([firstResponderView isKindOfClass:UITextView.class] || [firstResponderView isKindOfClass:UITextField.class]) {
+        return [self _cellForTextEdigintView:firstResponderView];
+    }
+    return nil;
+}
+
+- (UITableViewCell *)_cellForTextEdigintView:(UIView *)textEditingView {
+    UIView * view = textEditingView;
+    while (view != nil) {
+        if ([view isKindOfClass:UITableViewCell.class]) {
+            return (UITableViewCell *)view;
+        }
+        view = view.superview;
+    }
+    return nil;
+}
+
 
 @end
