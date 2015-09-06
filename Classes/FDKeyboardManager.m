f2cccfe4ce4b79b3feec0df5a6441d099837e738
Pierre Felgines <pierre.felgines@applidium.com>
2015-09-06 19:00:54 +0200

    Create empty FDKeyboardManager

---
+++ Classes/FDKeyboardManager.m
@@ -0,0 +1,28 @@
+//
+//  FDKeyboardManager.m
+//  FormDemo
+//
+//  Created by Pierre on 06/09/2015.
+//
+//
+
+#import "FDKeyboardManager.h"
+
+@implementation FDKeyboardManager
+
+- (instancetype)initWithTableView:(UITableView *)tableView {
+    if (self = [super init]) {
+
+    }
+    return self;
+}
+
+- (void)startObservingKeyboard {
+
+}
+
+- (void)endObservingKeyboard {
+
+}
+
+@end
