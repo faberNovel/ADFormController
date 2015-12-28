9935d30bdbeebaaa80b0ae665a4e8e18da7969a6
Pierre Felgines <pierre.felgines@applidium.com>
2015-12-28 10:34:56 +0100

    Create text field bindings

--- Modules/ADFormTableViewController/ADFormTextFieldTableViewCell.m
+++ Modules/ADFormTableViewController/ADFormTextFieldTableViewCell.m
@@ -10,25 +10,22 @@
 #import "ADTextFieldFormatter.h"
 #import "ADTextField.h"
 #import "ADFormCellConfiguration.h"
+#import "ADDatePickerTextFieldBinding.h"
+#import "ADPickerViewTextFieldBinding.h"
 
-@interface ADFormTextFieldTableViewCell () <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate> {
+@interface ADFormTextFieldTableViewCell () <UITextFieldDelegate> {
     NSMutableArray * _dynamicConstraints;
-    UIDatePicker * _datePicker;
-    UIPickerView * _pickerView;
     id<ADTextFieldFormatter> _textFieldFormatter;
-    NSDateFormatter * _dateFormatter;
-    id<ADFormPickerDataSource> _formPickerDataSource;
     ADTextField * _textField;
+    ADDatePickerTextFieldBinding * _datePickerBinding;
+    ADPickerViewTextFieldBinding * _pickerViewBinding;
 }
 
 @property (nonatomic) ADFormTextCellType cellType;
 @property (nonatomic, strong) UIView * rightView;
 @property (nonatomic, strong) UILabel * leftLabel;
 
-- (IBAction)_dateChanged:(UIDatePicker *)sender;
 - (IBAction)_textChanged:(UITextField *)textField;
-- (void)_startEditingDatePicker;
-- (void)_startEditingPickerView;
 
 @end
 
@@ -67,13 +64,8 @@ static NSString * kLeftLabelKeyPath = @"_leftLabel.text";
 
         [self addObserver:self forKeyPath:kLeftLabelKeyPath options:NSKeyValueObservingOptionNew context:NULL];
 
-        _datePicker = [[UIDatePicker alloc] init];
-        _datePicker.datePickerMode = UIDatePickerModeDate;
-        [_datePicker addTarget:self action:@selector(_dateChanged:) forControlEvents:UIControlEventValueChanged];
-
-        _pickerView = [[UIPickerView alloc] init];
-        _pickerView.dataSource = self;
-        _pickerView.delegate = self;
+        _datePickerBinding = [[ADDatePickerTextFieldBinding alloc] initWithTextField:self.textField];
+        _pickerViewBinding = [[ADPickerViewTextFieldBinding alloc] initWithTextField:self.textField];
     }
     return self;
 }
@@ -166,11 +158,11 @@ static NSString * kLeftLabelKeyPath = @"_leftLabel.text";
             self.textField.keyboardType = UIKeyboardTypeNumberPad;
         } break;
         case ADFormTextCellTypeDate: {
-            self.textField.inputView = _datePicker;
+            self.textField.inputView = _datePickerBinding.datePicker;
             _textField.disablePasteAction = YES;
         } break;
         case ADFormTextCellTypePicker: {
-            self.textField.inputView = _pickerView;
+            self.textField.inputView = _pickerViewBinding.pickerView;
             _textField.disablePasteAction = YES;
         } break;
         default:
@@ -189,10 +181,10 @@ static NSString * kLeftLabelKeyPath = @"_leftLabel.text";
 - (void)textFieldDidBeginEditing:(UITextField *)textField {
     switch (self.cellType) {
         case ADFormTextCellTypeDate: {
-            [self _startEditingDatePicker];
+            [_datePickerBinding startEditing];
         } break;
         case ADFormTextCellTypePicker: {
-            [self _startEditingPickerView];
+            [_pickerViewBinding startEditing];
         } break;
         default:
             break; // no op
@@ -236,8 +228,8 @@ static NSString * kLeftLabelKeyPath = @"_leftLabel.text";
     }
     _textFieldFormatter = configuration.textFieldFormatter;
     [_textFieldFormatter textFieldValueChanged:self.textField];
-    _dateFormatter = configuration.dateFormatter;
-    _formPickerDataSource = configuration.formPickerDataSource;
+    _datePickerBinding.dateFormatter = configuration.dateFormatter;
+    _pickerViewBinding.formPickerDataSource = configuration.formPickerDataSource;
 }
 
 - (void)beginEditing {
@@ -264,71 +256,12 @@ static NSString * kLeftLabelKeyPath = @"_leftLabel.text";
     return _textField.returnKeyType;
 }
 
-#pragma mark - UIPickerViewDataSource
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
-    return [_formPickerDataSource numberOfComponents];
-}
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    return [[_formPickerDataSource optionsForComponent:component] count];
-}
-
-#pragma mark - UIPickerViewDelegate
-
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
-    return [_formPickerDataSource optionsForComponent:component][row];
-}
-
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
-    NSMutableArray * selectedIndexes = [NSMutableArray array];
-    for (int component = 0; component < _pickerView.numberOfComponents; component++) {
-        [selectedIndexes addObject:@([_pickerView selectedRowInComponent:component])];
-    }
-
-    if (selectedIndexes.count) {
-        NSString * value = [_formPickerDataSource stringFromSelectedIndexes:selectedIndexes];
-        if (value.length) {
-            self.textField.text = @"";
-            [self.textField insertText:value];
-        }
-    }
-}
-
 #pragma mark - Private
-
-- (IBAction)_dateChanged:(UIDatePicker *)sender {
-    // Use insertText to simulate touch
-    self.textField.text = @"";
-    [self.textField insertText:[_dateFormatter stringFromDate:sender.date]];
-}
 
 - (IBAction)_textChanged:(UITextField *)textField {
     [_textFieldFormatter textFieldValueChanged:textField];
     if ([self.delegate respondsToSelector:@selector(textInputTableViewCellValueChanged:)]) {
         [self.delegate textInputTableViewCellValueChanged:self];
-    }
-}
-
-- (void)_startEditingDatePicker {
-    if (self.textField.text.length == 0) {
-        [self _dateChanged:_datePicker];
-    } else {
-        NSDate * date = [_dateFormatter dateFromString:self.textField.text];
-        _datePicker.date = date;
-    }
-}
-
-- (void)_startEditingPickerView {
-    if (self.textField.text.length == 0) {
-        for (int component = 0; component < _pickerView.numberOfComponents; component++) {
-            [self pickerView:_pickerView didSelectRow:0 inComponent:component];
-        }
-    } else {
-        NSArray * indexes = [_formPickerDataSource selectedIndexesFromString:self.textField.text];
-        [indexes enumerateObjectsUsingBlock:^(NSNumber * indexNumber, NSUInteger idx, BOOL * _Nonnull stop) {
-            [_pickerView selectRow:[indexNumber integerValue] inComponent:idx animated:NO];
-        }];
     }
 }
 
