09eab8303653e94ceff769cb42265d40cf59f664
Roland Borgese <roland.borgese@applidium.com>
2016-06-01 15:39:02 +0200

    create class ADFormSwitchTableViewCell

---
+++ Modules/ADFormTableViewController/ADFormSwitchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ADFormSwitchTableViewCell.h
+//  FormDemo
+//
+//  Created by Roland Borgese on 01/06/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADFormSwitchTableViewCell : UITableViewCell
+
+@end
---
+++ Modules/ADFormTableViewController/ADFormSwitchTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ADFormSwitchTableViewCell.m
+//  FormDemo
+//
+//  Created by Roland Borgese on 01/06/2016.
+//
+//
+
+#import "ADFormSwitchTableViewCell.h"
+
+@implementation ADFormSwitchTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
