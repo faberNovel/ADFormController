6e155d2ba46108e193e39d6ed977dfb10c94f35f
Samuel Gallet <samuel.gallet@applidium.com>
2016-07-04 12:25:10 +0200

    Create new swift project
    Change-Id: I76e2ef65bf74bc29bc785d63cee95b1f2ae7960b

---
+++ Classes/AppDelegate.swift
@@ -0,0 +1,73 @@
+//
+//  AppDelegate.swift
+//  FormDemo
+//
+//  Created by Edouard Siegel on 03/03/16.
+//
+//
+
+import HockeySDK
+import UIKit
+import Watchdog
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, BITHockeyManagerDelegate {
+
+    var window: UIWindow?
+    var watchdog: Watchdog?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        setupLogger()
+        setupHockeyApp()
+        setupWatchdog()
+
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        window?.rootViewController = SharedViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    //MARK: - BITHockeyManagerDelegate
+
+    func attachmentForCrashManager(crashManager: BITCrashManager!) -> BITHockeyAttachment! {
+        return BITHockeyAttachment(
+            filename: "report",
+            hockeyAttachmentData: Logger.sharedInstance.fileLogs(),
+            contentType: "text/plain"
+        )
+    }
+
+    //MARK: - Private
+
+    private func setupLogger() {
+        guard ADTargetSettings.sharedSettings().useFileLogger else {
+            return
+        }
+        Logger.sharedInstance.setup()
+    }
+
+    private func setupHockeyApp() {
+        if (ADTargetSettings.sharedSettings().hockeyAppId ?? "").isEmpty {
+            return
+        }
+        BITHockeyManager.sharedHockeyManager().configureWithIdentifier(ADTargetSettings.sharedSettings().hockeyAppId)
+        BITHockeyManager.sharedHockeyManager().crashManager.crashManagerStatus = .AutoSend
+        BITHockeyManager.sharedHockeyManager().crashManager.enableAppNotTerminatingCleanlyDetection = true
+        if ADTargetSettings.sharedSettings().useFileLogger {
+            BITHockeyManager.sharedHockeyManager().delegate = self
+        }
+        BITHockeyManager.sharedHockeyManager().startManager()
+    }
+
+    private func setupWatchdog() {
+        if (!ADTargetSettings.sharedSettings().useWatchdog) {
+            return;
+        }
+        watchdog = Watchdog(threshold: 0.2, handler: { (duration) -> () in
+            DDLogWarn("[Watchdog] Block main thread for \(duration)s");
+        })
+    }
+}
--- Podfile
+++ Podfile
@@ -1,10 +1,32 @@
 source 'git@scm.applidium.net:CocoaPodsSpecs.git'
 source 'https://github.com/CocoaPods/Specs.git'
 
-platform :ios, '7.0'
+platform :ios, '8.0'
+use_frameworks!
 
-pod 'CocoaLumberjack', '~> 2.2.0'
-pod 'ADDynamicLogLevel', '~> 1.1.0'
-pod 'ADKeyboardManager', '~> 2.0.0'
+pod 'CocoaLumberjack/Swift', '~>  2.0', :inhibit_warnings => true
+pod 'ADDynamicLogLevel', '~>  1.1', :inhibit_warnings => true
 
-target 'FormDemo'
+target 'FormDemo' do
+  pod 'Alamofire', '~> 3.0'
+  pod 'HockeySDK', '~> 3.8', :subspecs => ['CrashOnlyLib']
+  pod 'Watchdog', '~> 1.0'
+end
+
+target 'FormDemoTests' do
+  pod 'Quick', '~> 0.9'
+  pod 'Nimble', '~> 4.0'
+  pod 'Nimble-Snapshots', '~> 4.1'
+  pod 'OCMock', '~> 3.3'
+  pod 'FBSnapshotTestCase', '~> 2.1'
+end
+
+post_install do |installer|
+    installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+            config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ""
+            config.build_settings['CODE_SIGNING_REQUIRED'] = "NO"
+            config.build_settings['CODE_SIGNING_ALLOWED'] = "NO"
+        end
+    end
+end
