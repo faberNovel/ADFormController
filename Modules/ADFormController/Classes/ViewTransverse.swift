7d4a099a6475ee66b12a2fe5fdf49cd30f2ab126
Samuel Gallet <samuel.gallet@applidium.com>
2016-07-08 12:09:05 +0200

    Add method to UIViewTransverse
    Change-Id: I07c9a822f33a17b4174dcee429bc259f77eaeaa4

--- Modules/ADFormController/Classes/ViewTransverse.swift
+++ Modules/ADFormController/Classes/ViewTransverse.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 public extension UIView {
-    public func superView<T: UIView>() -> T? {
+    public func superViewFromClass<T: UIView>() -> T? {
         var view: UIView = self
         guard let castedView = view as? T else {
             return view.superview.flatMap {
-                return $0.superView()
+                return $0.superViewFromClass()
             }
         }
         return castedView
+    }
+
+    public func findFirstResponder() -> UIView? {
+        if isFirstResponder() {
+            return self
+        }
+        for subview in subviews {
+            if let firstResponser = subview.findFirstResponder() {
+                return firstResponser
+            }
+        }
+        return nil
     }
 }
